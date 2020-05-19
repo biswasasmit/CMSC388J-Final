d8b924b07d4b9b06c0500749a299d114840fd50c
Asmit Biswas <biswasasmit@gmail.com>
2020-05-19 00:02:04 -0400

    beautiful soup

--- flask_app/main/routes.py
+++ flask_app/main/routes.py
@@ -1,19 +1,30 @@
 ## Main routes
-
+from bs4 import BeautifulSoup
 from flask import render_template, redirect, url_for, Response, Blueprint, flash
 from ..forms import SearchForm, InviteFriendForm
 from .. import client
+from ..utils import current_time
+import requests
 
 main = Blueprint("main", __name__)
+
+def find_top_ten():
+    top_ten = []
+    steampage = BeautifulSoup(requests.get('https://store.steampowered.com/stats/').text, "html.parser")
+    for row in steampage('tr', {'class' :'player_count_row', 'style' :''}):
+        x = row.text.split('\n')
+        top_ten.append((x[2],x[5],x[9]))
+    return top_ten
+
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = SearchForm()
-
+    top_ten = find_top_ten()
     if form.validate_on_submit():
         return redirect(url_for('main.query_results', query=form.search_query.data))
 
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form,top_ten = top_ten, date = current_time())
 
 @main.route('/search-results/<query>', methods=['GET'])
 def query_results(query):
@@ -39,3 +50,5 @@ def invite():
         return redirect(url_for('main.invite'))
     
     return render_template("invite.html", invite_form=invite_form)
+
+find_top_ten()
