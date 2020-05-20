5701b823387ce1852958b809ba5352bc243f9e8e
Asmit Biswas <biswasasmit@gmail.com>
2020-05-20 03:44:09 -0400

    profile_pic

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -35,7 +35,10 @@ csp = {
         'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
         'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'
         ],
-    'img-src': '*',
+    'img-src': [
+        '*',
+        '\'self\''
+        ],
     'script-src' : [
         'https://code.jquery.com/jquery-3.4.1.slim.min.js', 
         'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
